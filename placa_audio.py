51ed8791ed8b2d8e5200fb54b5db105ab7993eee
agustinlp <agustin.lopezpedroso@gmail.com>
2019-05-09 15:59:41 -0300

    Modificación para agregar el control de osciloscopio y el generador de funciones

--- placa_audio.py
+++ placa_audio.py
@@ -4,7 +4,9 @@ Created on Sun Apr 28 00:04:42 2019
 
 @author: Agustin
 """
+import visa
 
+import time
 import pyaudio
 import numpy as np
 import wave 
@@ -31,6 +33,7 @@ class PlacaAudio():
         
         
     def record(self,duracion,sr=44100,CHUNK=1024,FORMAT = pyaudio.paInt16, CHANNELS = 2):
+        # Tengo dos canales
         self.stream = self.p.open(format=FORMAT, 
                                   channels=CHANNELS, 
                                   rate=sr, 
@@ -48,16 +51,113 @@ class PlacaAudio():
         
         self.stream.stop_stream()
         self.stream.close()
-        return np.fromstring(b''.join(frames),dtype=np.int16)
+        tiempo = np.arange(0,duracion,1/40960)
+        return tiempo, np.fromstring(b''.join(frames),dtype=np.int16)
     
     def play_sin(self, frec, duration=2, volume=0.5, sr=44100):
         length = sr*duration
         seno = (np.sin(2*np.pi*np.arange(length)*frec/sr)).astype(np.float32)
         self.stream = self.p.open(format=pyaudio.paFloat32,channels=1,rate=sr,output=True)
         self.stream.write(volume*seno)
+    
+
+
         
     def sweep_sin(self, fi, ff, step, duration=3, volume =0.5):
         frecs = np.linspace(fi,ff,step)
         for i in frecs:
             self.play_sin(i,duration, volume)
+            print(i)
+
+
+
+#Control del osciloscopio      
+class TekGen:
+    
+    # Me defino un template para los generadores de funciones de tektronix.
+    # Para crear un generador, se necesita proveer el resource manager,
+    # y la posición que ocupa el instrumento en la lista de list.resources().
+    
+    def __init__(self, rm, num):
+        # Si el generador de funciones responde, se escucha un beep al inicia_
+        # lizarlo.
+        data = rm.list_resources()
+        self.inst = rm.open_resource('{}'.format(data[num]))
+        self.inst.write('SYSTEM:BEEPER:IMM')        
         
+    def identity(self):
+        print(self.inst.query('*IDN?'))
+    
+    def set_freq(self, hertz):
+        self.inst.write("SOUR1:FREQ:FIX {}".format(hertz))
+        
+    def set_amp(self, volts):
+        self.inst.write("SOUR1:VOLT:AMPL {}".format(volts))
+    
+    def waveform_shape(self, channel = 1, shape = 'SIN'):
+        # Formas posibles: SIN, SQU, PULS, RAMP
+        # PRNoise, DC|SINC|GAUSsian|LORentz|ERISe|EDECay|
+        # HAVersine
+        self.inst.write('SOUR{}:FUNC:SHAPE {}'.format(channel, shape))
+        
+    def waveform_phase(self, radians, channel = 1):
+        self.inst.write('SOUR{}:PHAS {})'.format(channel, radians))
+    
+    def freq_sweep(self, freq_inicial, freq_final, step, stop = 1, channel = 1):
+        # Hace un barrido de frecuencias desde freq_inicial hasta freq_final
+        # con un step. Entre cada cambio de frecuencia dejamos 1 segundo de
+        # stop.
+        frecuencias = np.arange(freq_inicial, freq_final, step)
+        for elemento in frecuencias:
+            self.inst.write('SOUR{}:FREQ:FIX {}'.format(channel, elemento))
+            time.sleep(stop)
+
+class Osciloscopio:
+    
+    def __init__(self,rm,num):
+        data = rm.list_resources()
+        self.inst = rm.open_resource('{}'.format(data[num]))
+        self.parameters = None
+        
+    def identity(self):
+        #Devuelve el nombre del instrumento según el fabricante.
+        name = self.inst.query("*IDN?")
+        print("Name of this device: {}".format(name))
+    
+    def get_parameters(self):
+        self.parameters = self.inst.query_ascii_values('WFMPRE:XZE?;XIN?;YZE?;YMU?;YOFF?;', separator=';')
+        
+#        if self.parameters is None:
+#            self.parameters = self.inst.query_ascii_values('WFMPRE:XZE?;XIN?;YZE?;YMU?;YOFF?;', separator=';')
+
+    
+    def curva(self):
+        self.get_parameters()
+        xze, xin, yze, ymu, yoff = self.parameters
+        data = self.inst.query_ascii_values("CURV?",container=np.array)
+        tiempo = xze + np.arange(len(data)) * xin
+        data = (data-yoff)* ymu + yoff
+        return tiempo, data
+
+
+def input_placa(gen, placa,freq_inicial, freq_final, step, stop = 1, channel = 1):
+        # Hace un barrido de frecuencias desde freq_inicial hasta freq_final
+        # con un step. Entre cada cambio de frecuencia dejamos 1 segundo de
+        # stop.
+        frecuencias = np.arange(freq_inicial, freq_final, step)
+        salida = []
+        for elemento in frecuencias:
+            gen.set_freq(elemento)
+            salida.extend(placa.record(0.05, CHANNELS=1)[1])
+            time.sleep(stop)
+        
+        return salida
+
+rm = visa.ResourceManager()
+data = rm.list_resources()
+
+#%%
+
+with open('resultados.txt', 'w') as f:
+    for renglon in result:
+        f.write(str(renglon))
